c2fcaf9adfb86e23a4649ee7c26b96bb22f5b7d9
Filipe Soares <filipecarlossoares@gmail.com>
2019-03-24 10:14:46 -0300

    Mostrando categorias

--- src/app/categorias/categorias.page.ts
+++ src/app/categorias/categorias.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from 'src/services/domain/categoria.service';
+import { CategoriaDTO } from 'src/models/cotegoria.dto';
+import { API_CONFIG } from 'src/config/api.config';
 
 @Component({
   selector: 'app-categorias',
@@ -7,12 +9,14 @@ import { CategoriaService } from 'src/services/domain/categoria.service';
   styleUrls: ['./categorias.page.scss'],
 })
 export class CategoriasPage implements OnInit {
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
 
   constructor(public categoriaService: CategoriaService) { }
 
   ngOnInit() {
     this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     }, error => {
       console.log(error);
     })
