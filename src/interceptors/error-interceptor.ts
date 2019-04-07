d41eca6f88d02e2c26d4f184902a9bdd22474cd3
Filipe Soares <filipecarlossoares@gmail.com>
2019-04-06 21:57:59 -0300

    Interceptor para incluir token nas requisicoes

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,34 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HTTP_INTERCEPTORS
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { retry, catchError } from "rxjs/operators";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      retry(1),
+      catchError((error, caught) => {
+        let errorObj = error;
+        if (errorObj.error) {
+          errorObj = errorObj.error;
+        }
+        if (!errorObj.status) {
+          errorObj = JSON.parse(errorObj);
+        }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Passou no interceptor");
-        return next.handle(req).pipe(retry(1),
-        catchError((error, caught) => {
+        console.log("Erro detectado pelo interceptor:");
+        console.log(errorObj);
 
-            let errorObj = error;
-            if (errorObj.error) {
-                errorObj = errorObj.error;
-            }
-            if (!errorObj.status) {
-                errorObj = JSON.parse(errorObj);
-            }
-
-            console.log("Erro detectado pelo interceptor:");
-            console.log(errorObj);
-
-            return Observable.throw(errorObj);
-        })) as any;
-    }
+        return Observable.throw(errorObj);
+      })
+    ) as any;
+  }
 }
 
 export const ErrorInterceptorProvider = {
-    provide: HTTP_INTERCEPTORS,
-    useClass: ErrorInterceptor,
-    multi: true,
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
 };
